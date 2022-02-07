fc68482d7099c868664d682257ee22949ab4c564
Thameem Ansari <97792702+loyalty-thameem@users.noreply.github.com>
2022-02-07 19:05:55 +0530

    Create Paginator.js

---
+++ Paginator.js
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import { View, Text, Button,Icon,StyleSheet,FlatList, Image,useWindowDimensions,Animated } from 'react-native';
+import SlidesData from '../SlidesData';
+
+const Paginator = ({data,scrollX}) => {
+ const {width} = useWindowDimensions();
+  return (
+      <View style={{flexDirection:'row',height:64}}>
+      {data.map((_,i)=>{
+        const inputRange =[(i-1)*width,i*width,(i+1)*width];
+        const dotWidth =scrollX.interpolate({
+          inputRange,
+          outputRange:[10,20,10],
+          extrapolate:'clamp'//... dot before after not hide on view  
+        });
+        //Dot color functionalty.color for output range but output value gray something on view.
+        const opacity =scrollX.interpolate({
+          inputRange,
+          outputRange:[0.3,1,0.3],
+          extrapolate:'clamp'//... dot before after not hide on view 
+        })
+        return <Animated.View 
+                            style={[styles.dot,
+                                          {
+                                            width:dotWidth,opacity
+                                          }
+                                          ]}
+                                           key={i.toString()
+                                           }
+                                           />
+      })}
+      </View>
+
+
+  );
+}
+export default Paginator
+const styles=StyleSheet.create({
+dot:{
+   height:10,
+    backgroundColor:'#493d8a',
+    marginHorizontal:8,
+    borderRadius:5,
+  },
+})
